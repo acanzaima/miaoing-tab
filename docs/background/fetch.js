52bead405ab08ca1ded0334cfa3aea81972bbefb
starrysky <1039248213@qq.com>
2026-06-26 22:48:30 +0800

    release: version 0.0.13

--- docs/background/fetch.js
+++ docs/background/fetch.js
@@ -1,6 +1,20 @@
 
+const readResponseText = async response => {
+  const needTransCharset = ['charset=gbk', 'charset=GBK']
+  const contentType = response.headers.get('content-type') || ''
+  const needTrans = needTransCharset.some(i => contentType.includes(i))
+
+  if (needTrans) {
+    const res = await response.arrayBuffer()
+    const decoder = new TextDecoder('gbk')
+    return decoder.decode(res)
+  }
+
+  return response.text()
+}
+
 // fetch函数
- function fetchSearchSuggestions(url, returnResponse) {
+function fetchSearchSuggestions(url, returnResponse) {
   return new Promise(async(resolve, reject) => {
     try {
       // 发起跨域请求
@@ -19,18 +33,7 @@
       if (returnResponse) {
         resolve(response)
       } else {
-        const needTransCharset = ['charset=gbk', 'charset=GBK']
-        const contentType  = response.headers.get('content-type')
-        const needTrans = needTransCharset.some(i => contentType?.includes(i))
-        if (needTrans) {
-          const res = await response.arrayBuffer();
-          const decoder = new TextDecoder('gbk');
-          const resText = decoder.decode(res);
-          resolve(resText)
-        } else {
-          const res = await response.text()
-          resolve(res)
-        }
+        resolve(await readResponseText(response))
       }
 
     } catch (error) {
@@ -42,6 +45,29 @@
 }
 
 // 监听来自内容脚本的消息
+function fetchNetworkBlob(url) {
+  return fetchSearchSuggestions(url, true).then(async res => {
+    const buffer = await res.arrayBuffer()
+    if (buffer.byteLength > 50 * 1024 * 1024) {
+      throw new Error('Network resource is too large')
+    }
+    return {
+      contentType: res.headers.get('content-type') || 'application/octet-stream',
+      data: Array.from(new Uint8Array(buffer))
+    }
+  })
+}
+
+function fetchTextResource(url) {
+  return fetchSearchSuggestions(url, true).then(async res => {
+    return {
+      contentType: res.headers.get('content-type') || '',
+      data: await readResponseText(res),
+      finalUrl: res.url || url
+    }
+  })
+}
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'FETCH_SEARCH_SUGGESTIONS') {
     const { url } = request.payload;
@@ -83,16 +109,56 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
     // 表示将异步发送响应
     return true;
+  } else if (request.type === 'FETCH_TEXT_RESOURCE') {
+    const { url } = request.payload;
+
+    fetchTextResource(url)
+      .then(res => {
+        sendResponse({
+          success: true,
+          contentType: res.contentType,
+          data: res.data,
+          finalUrl: res.finalUrl
+        });
+      })
+      .catch(error => {
+        sendResponse({
+          success: false,
+          error: error
+        });
+      });
+
+    return true;
+  } else if (request.type === 'FETCH_WALLPAPER_BLOB') {
+    const { url } = request.payload;
+
+    fetchNetworkBlob(url)
+      .then(res => {
+        sendResponse({
+          success: true,
+          contentType: res.contentType,
+          data: res.data
+        });
+      })
+      .catch(error => {
+        sendResponse({
+          success: false,
+          error: error
+        });
+      });
+
+    return true;
   } else if (request.type === 'FEICH_HOST_FAVICON') {
     const { url } = request.payload;
 
     // 使用 Promise 处理异步请求
-    fetchSearchSuggestions(url, true)
-      .then(async res => {
-        const htmlText = await res.text(); 
+    fetchTextResource(url)
+      .then(res => {
         sendResponse({
           success: true, 
-          data: htmlText
+          data: res.data,
+          finalUrl: res.finalUrl,
+          contentType: res.contentType
         });
       })
       .catch(error => {
